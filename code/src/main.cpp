96b98ed546c8b07e41b3d4fd180b9c6527431c5e
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2015-06-16 12:20:24 +0800

    Updated submodules, CMakeLists.txt and main

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,11 +1,55 @@
-//#include <neural_network.hpp>
-//#include <car.hpp>
-//#include <world.hpp>
-
-#include <iostream>
+#include <car.hpp>
+#include <renderer.hpp>
+#include <staticbox.hpp>
+#include <world.hpp>
 
 int main()
 {
-    std::cout << "Neuro car" << std::endl;
+    uint32_t worldWidth = 100;
+    uint32_t worldHeight = 80;
+
+    #if NEURO_CAR_GRAPHIC_MODE
+    Renderer r(8, worldWidth, worldHeight);
+    World w(8, 3, &r);
+    #else
+    World w(8, 3);
+    #endif
+
+    w.addBorders(worldWidth, worldHeight);
+
+    w.randomize(worldWidth, worldHeight, 15);
+
+
+    // a car
+
+    std::vector<float32> angles;
+    //angles to ray cast
+    angles.push_back(0.0f);
+    angles.push_back(b2_pi);
+
+    angles.push_back(b2_pi/2.0f);
+    angles.push_back(-b2_pi/2.0f);
+
+    angles.push_back(b2_pi/4.0f);
+    angles.push_back(-b2_pi/4.0f);
+
+    angles.push_back(b2_pi/8.0f);
+    angles.push_back(-b2_pi/8.0f);
+
+    angles.push_back(3.0f*b2_pi/8.0f);
+    angles.push_back(-3.0f*b2_pi/8.0f);
+
+    static auto const toRadian = [](float32 degree)
+    {
+        return degree * M_PI / 180.0;
+    };
+
+    float32 carAngle = 90.0;
+    Car* car = new Car(b2Vec2(50, 10), toRadian(carAngle), 2, 3, 8.0, angles);
+
+    w.addDrawable(car);
+
+    w.run();
+
     return 0;
 }
